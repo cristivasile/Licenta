832e1b610945d4bb02e49703635e1aa4f9c4e269
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2023-03-18 14:37:52 +0200

    Refactoring

--- Frontend/car-market-app/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const userLocalStoragePath = "user";
-export const tokenLocalStoragePath = "token";
-export const roleLocalStoragePath = "role";
-export const apiUrl = "http://localhost:5000";
