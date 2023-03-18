68f79ee9524c3fad252872a20a6cd76d18bdc74f
Vasile George-Cristian <79210106+cristivasile@users.noreply.github.com>
2023-03-18 14:37:28 +0200

    Added sorting to features

--- Frontend/car-market-app/src/redux/featuresStore.tsx
+++ Frontend/car-market-app/src/redux/featuresStore.tsx
@@ -1,6 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { FeatureModel, mapJsonToFeatureModels } from "../models/FeatureModel";
 
+function sortFeatures(input: FeatureModel[]): void {
+    input.sort((x1, x2) => x1.name < x2.name ? -1 : 1);
+}
+
 export const featureSlice = createSlice({
     name: "featureState",
     initialState: {
@@ -9,9 +13,11 @@ export const featureSlice = createSlice({
     reducers: {
         setFeaturesFromJson: (state, action) => {
             state.features = mapJsonToFeatureModels(action.payload);
+            sortFeatures(state.features);
         },
         addFeature: (state, action) => {
             state.features.push(action.payload);
+            sortFeatures(state.features);
         },
         removeFeatureById: (state, action) => {
             state.features = state.features.filter(feature => feature.id !== action.payload.id);
@@ -21,6 +27,7 @@ export const featureSlice = createSlice({
                 if (state.features[i].id === action.payload.id) {
                     state.features[i].name = action.payload.updatedName;
                 }
+            sortFeatures(state.features);
         }
     },
 });
